91c7cd8d35f00c13c295e2b47cbc5a9e811af797
Jessika Babeux <jessikahurst23@gmail.com>
2020-10-05 14:51:59 -0400

    Laboratoire terminé -menu hamburger -hover -nav-mobile

--- src/js/script.js
+++ src/js/script.js
@@ -2,5 +2,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     //Un commentaire
     console.log("Ça fonctionne!!!");
+    var hamburgers = document.querySelectorAll('.hamburger');
+    var menuMoblile = document.querySelector(".nav-primary-mobile");
+
+    for(let i=0 ; i<hamburgers.length; i++){
+        var hamburger = hamburgers[i];
+        hamburger.addEventListener('click', openMenu);
+    }
+
+    function openMenu(evt) {
+        evt.preventDefault();
+
+        var cible = evt.currentTarget;
+
+        if(cible.classList.contains('open')){
+            cible.classList.remove('open');
+            menuMoblile.classList.remove('open');
+        }else{
+            cible.classList.add('open');
+            menuMoblile.classList.add('open');
+        }
+    }
 
 });
